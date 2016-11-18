1f2ab5bd7039f82f174166cfff62993b099b113b
martina-zxy <martina.megasari@gmail.com>
2016-11-18 23:27:31 +0100

    View

--- FirebaseTest/src/main/java/TestData.java
+++ FirebaseTest/src/main/java/TestData.java
@@ -1,5 +1,21 @@
 
 public class TestData {
+	public String getTestName() {
+		return testName;
+	}
+
+	public void setTestName(String testName) {
+		this.testName = testName;
+	}
+
+	public Object getTestTime() {
+		return testTime;
+	}
+
+	public void setTestTime(Object testTime) {
+		this.testTime = testTime;
+	}
+
 	public String testName;
 	public Object testTime;
 	
